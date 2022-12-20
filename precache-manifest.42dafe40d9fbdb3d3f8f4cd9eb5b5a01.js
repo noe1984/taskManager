a13870529fc0fedf394e424436f9c179a7bc6e1e
Noe Gonzalez <palmas.inter@gmail.com>
2022-12-19 23:21:18 -0800

    Updates

---
+++ precache-manifest.42dafe40d9fbdb3d3f8f4cd9eb5b5a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66410319da298a16b565",
+    "url": "/taskManager/static/css/main.8a666105.chunk.css"
+  },
+  {
+    "revision": "66410319da298a16b565",
+    "url": "/taskManager/static/js/main.66410319.chunk.js"
+  },
+  {
+    "revision": "ffd6d87c7f4543eb2397",
+    "url": "/taskManager/static/js/1.ffd6d87c.chunk.js"
+  },
+  {
+    "revision": "3a251e458a97ad470248",
+    "url": "/taskManager/static/js/runtime~main.3a251e45.js"
+  },
+  {
+    "revision": "d11ccb1d6a8e43edfd5dcb0e559cf43f",
+    "url": "/taskManager/index.html"
+  }
+];
